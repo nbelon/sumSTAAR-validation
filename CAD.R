cbd6571c04d283e7d3cc17a22edc89040627c9be
nbelon <62548352+nbelon@users.noreply.github.com>
2022-04-09 21:24:58 +0700

    Update CAD.R
    Code for Q-Q plots added

--- CAD.R
+++ CAD.R
@@ -1,4 +1,4 @@
-### a script used to generate Figures S3, S4
+### a script used to generate Figures S3, S4, S5
 # Nadezhda Belonogova, Irina Zorkoltseva 2022
 
 ##### STAAR null model
@@ -7,7 +7,7 @@ library(Matrix)
 library(STAAR)
 library(data.table)
 
-phen <- fread('/home/lima/Zor/STAAR/CAD/I25cov.nonrel.ord.csv', h = T, data.table = F)
+phen <- fread('/home/lima/Zor/SUM_STATISTICS/sumSTAAR/CAD/I25cov.nonrel.ord.csv', h = T, data.table = F)
 # UK Biobank phenotype of the Chronic ischaemic heart disease (IHD, ICD-10 code I25)
 # containing 153,379 unrelated individuals (12,931 cases / 140,448 controls) with European ancestry (project #59345)
 
@@ -33,7 +33,7 @@ ndose =M%/%nvar
 for (i in 1:ndose) {
 start <-(i-1)*nvar + 1
 end <- start + nvar - 1
-Geno <- read.plink('/home/lima/Zor/STAAR/CAD/white_nonrel_chr01',select.snps = c(start:end))
+Geno <- read.plink('/home/lima/Zor/SUM_STATISTICS/sumSTAAR/CAD/white_nonrel_chr01',select.snps = c(start:end))
 gt <- as(Geno$genotype,'numeric')
 pval <- Indiv_Score_Test_Region(genotype=gt,obj_nullmodel,rv_num_cutoff = 5,rare_maf_cutoff = 1)
 ind.pval <- rbind (ind.pval,pval)
@@ -43,7 +43,7 @@ print(paste('i=',i,'from ',start,' to ',end))
 
 ##Last dose
 start = nvar*ndose+1
-Geno <- read.plink('/home/lima/Zor/STAAR/CAD/white_nonrel_chr01',select.snps = c(start:M))
+Geno <- read.plink('/home/lima/Zor/SUM_STATISTICS/sumSTAAR/CAD/white_nonrel_chr01',select.snps = c(start:M))
 gt <- as(Geno$genotype,'numeric')
 pval <- Indiv_Score_Test_Region(genotype=gt,obj_nullmodel,rv_num_cutoff = 5,rare_maf_cutoff = 1)
 ind.pval <- rbind (ind.pval,pval)
@@ -51,7 +51,7 @@ print(paste('end of last dose', start,M))
 write.table(pval, file = 'pval.all.staar.chr01.tmp', row.names = F, qu = F, sep=' ', append = T, col.names = F)
 
 print('read map file')
-map <- fread('/home/lima/Zor/STAAR/CAD/white_nonrel_chr01.bim', data = F, h = F)
+map <- fread('/home/lima/Zor/SUM_STATISTICS/sumSTAAR/CAD/white_nonrel_chr01.bim', data = F, h = F)
 map <- map[,-c(3)]
 colnames (map)[1:5] <- c('CHR','SNP','POS','A1','A2')
 total <- cbind(map,ind.pval)
@@ -60,8 +60,8 @@ write.table(total, file = 'ind.pval.all.staar.chr01.txt', row.names = F, qu = F,
 
 ####### Figure S3: fastGWA vs STAAR individual scores
 
-staar <- read.table('/home/lima/Zor/STAAR/staar/chr01/ind.pval.all.staar.chr01.txt', h = T)
-fast <- read.table('/home/lima/Zor/STAAR/staar/chr01/fast4gb_mac5.maf_0.01.txt', h = T)
+staar <- read.table('/home/lima/Zor/SUM_STATISTICS/sumSTAAR/staar/chr01/ind.pval.all.staar.chr01.txt', h = T)
+ fast <- read.table('/home/lima/Zor/SUM_STATISTICS/sumSTAAR/staar/chr01/fast4gb_mac5.maf_0.01.txt', h = T)
 # CAD summary statistics from fastGWA, calculated from the UK Biobank whole exome sequencing data and phenotype of the Chronic ischaemic heart disease (IHD, ICD-10 code I25)
 # containing 153,379 unrelated individuals (12,931 cases / 140,448 controls) with European ancestry (project #59345)
 
@@ -95,12 +95,14 @@ dev.off()
 fast <- read.table('fast4gb_mac5.maf_0.01.txt', h = T)
 
 fast <- fast[fast$N == 153375, ] # genotypes all measured
+dim(fast)[1]
+#[1] 105310
 
 fast$zt <- fast$T/fast$SE_T
 fast$pz <- 2*pnorm(abs(fast$zt),low=F)
 
-gwas <- fast[, c('CHR', 'SNP', 'POS', 'A1', 'AF1', 'BETA', 'SE', 'pz')]###fast
-colnames(gwas)[1:8] <- c('CHR','ID','POS','EA','EAF','BETA','SE','P')###fast
+gwas <- fast[, c('CHR', 'SNP', 'POS', 'A1', 'AF1', 'BETA', 'SE', 'pz')]
+colnames(gwas)[1:8] <- c('CHR','ID','POS','EA','EAF','BETA','SE','P')
 
 write.table(gwas, file = 'fastT.all.measured.txt', row = F, qu = F)
 
@@ -135,7 +137,7 @@ gens <- gens[gens$V3 =='chr1',c(1,5,6)]
 colnames(gens)[1:3] <- c('gene','start','end')
 
 # take SNPs with call rate = 1
-list <- fread('~/Zor/STAAR/staar/chr01/snplist4gb_mac5.maf_0.01.txt',h = T, data.table = F)
+list <- fread('~/Zor/SUM_STATISTICS/sumSTAAR/staar/chr01/snplist4gb_mac5.maf_0.01.txt',h = T, data.table = F)
 fast <- read.table('fastT.all.measured.txt', h = T)
 list <- list[list$SNP %in% fast$ID, ]
 dim(list)
@@ -152,7 +154,7 @@ for (i in 1:dim(gens)[1]) {
 
 	if (length(snps)> 1){
 		print(paste(i, g, length(snps)))
-		Geno <- read.plink('/home/lima/Zor/STAAR/CAD/white_nonrel_chr01',select.snps = snps)
+		Geno <- read.plink('/home/lima/Zor/SUM_STATISTICS/sumSTAAR/CAD/white_nonrel_chr01',select.snps = snps)
 		gt <- as(Geno$genotype,'numeric')
 		pval <- STAAR(genotype=gt,obj_nullmodel,annotation_phred=NULL,rare_maf_cutoff = 1)
 		pval_gen <- c()
@@ -204,6 +206,7 @@ legend('topleft', legend = paste0('r = ', format(round(cor, 6), nsmall = 3)), pc
 x <- -log10(as.numeric(res0[, c('STAAR.S11', 'STAAR.S125')]))
 y <- -log10(as.numeric(res0[, c('sumSTAAR.S11', 'sumSTAAR.S125')]))
 cor <- cor(x, y)
+lim <- ceiling(max(c(x, y)))
 plot(x, y, ylim = c(0, lim), xlim = c(0, lim), xlab = 'STAAR', ylab = 'sumFREGAT', main = 'SKAT')
 abline(0, 1)
 abline(reg = lm(y ~ x), col = 'red')
@@ -212,6 +215,7 @@ legend('topleft', legend = paste0('r = ', format(round(cor, 6), nsmall = 3)), pc
 x <- -log10(as.numeric(res0[, c('STAAR.A11', 'STAAR.A125')]))
 y <- -log10(as.numeric(res0[, c('sumSTAAR.A11', 'sumSTAAR.A125')]))
 cor <- cor(x, y)
+lim <- ceiling(max(c(x, y)))
 plot(x, y, ylim = c(0, lim), xlim = c(0, lim), xlab = 'STAAR', ylab = 'sumFREGAT', main = 'ACAT')
 abline(0, 1)
 abline(reg = lm(y ~ x), col = 'red')
@@ -220,6 +224,7 @@ legend('topleft', legend = paste0('r = ', format(round(cor, 6), nsmall = 3)), pc
 x <- -log10(as.numeric(res0[, 'STAAR.ACAT_O']))
 y <- -log10(as.numeric(res0[, 'sumSTAAR.ACAT_O']))
 cor <- cor(x, y)
+lim <- ceiling(max(c(x, y)))
 plot(x, y, ylim = c(0, lim), xlim = c(0, lim), xlab = 'STAAR', ylab = 'sumFREGAT', main = 'Combined tests')
 abline(0, 1)
 abline(reg = lm(y ~ x), col = 'red')
@@ -241,9 +246,9 @@ fast$pz <- staar$pval[v]
 
 fast <- fast[fast$SNP %in% staar$SNP, ]
 
-#gwas <- fast[,c(1:4,7,11,12,17)]###fast
-gwas <- fast[, c('CHR', 'SNP', 'POS', 'A1', 'AF1', 'BETA', 'SE', 'pz')]###fast
-colnames(gwas)[1:8] <- c('CHR','ID','POS','EA','EAF','BETA','SE','P')###fast
+#gwas <- fast[,c(1:4,7,11,12,17)]
+gwas <- fast[, c('CHR', 'SNP', 'POS', 'A1', 'AF1', 'BETA', 'SE', 'pz')]
+colnames(gwas)[1:8] <- c('CHR','ID','POS','EA','EAF','BETA','SE','P')
 
 write.table(gwas, file = 'staar.p.txt', row = F, qu = F)
 
@@ -272,3 +277,73 @@ p2 <- -log10(c(df$ACAT.1.1.PROB0, df$ACAT.1.25.PROB0))
 cor <- cor(p1[!is.na(p2)],p2[!is.na(p2)])
 cor
 #[1] 0.9985428
+
+################ Figure S5: qqplots
+
+df1 <- read.table('gb.staar.allmeas.out')
+colnames(df1) <- c('gene', 'nvar', 'STAAR.STAAR_O', 'STAAR.ACAT_O', 'STAAR.S11', 'STAAR.S125', 'STAAR.B11', 'STAAR.B125', 'STAAR.A11', 'STAAR.A125')
+load('all.meas.RDa')
+colnames(res) <- c('gene', 'sumSTAAR.A11', 'sumSTAAR.A125', 'sumSTAAR.B11', 'sumSTAAR.B125', 'sumSTAAR.S11', 'sumSTAAR.S125', 'sumSTAAR.ACAT_O')
+res <- res[which(!is.na(res$sumSTAAR.B11)), ]
+
+res0 <- merge (df1,res,by='gene')
+dim(res0)
+#[1] 1927   17
+sum(res0$nvar)
+# 110538
+res0$gene <- NULL
+res0 <- as.matrix(res0)
+
+ylb <- expression(paste(-log[10], "(observed P)"))
+xlb <- expression(paste(-log[10], "(expected P)"))
+
+draw.qq <- function(pval0, pval1, main) {
+
+	chi0 <- qchisq((1 - pval0), 1)
+	lam0 <- median(chi0)/qchisq(0.5, 1)
+	chi1 <- qchisq((1 - pval1), 1)
+	lam1 <- median(chi1)/qchisq(0.5, 1)
+
+	pval0 <- -log10(pval0)
+	pval1 <- -log10(pval1)
+
+	n <- length(pval0)
+	a <- 1:n
+	b <- a/n
+	x <- -log10(b)
+
+	upper <- qbeta(0.025, a, rev(a))
+	lower <- qbeta(0.975, a, rev(a))
+
+	plot(x, pval0, type = "n", ylab = ylb, xlab = xlb, ylim = c(0, ceiling(max(c(pval0, pval1)))), main = main)
+	# upper and lower have already been subset
+	polygon(-log10(c(b, rev(b))), -log10(c(upper, rev(lower))), density=NA, col="gray80")
+	points(x, pval0, pch = 19, col = 'black')
+	points(x, pval1, pch = 19, col = 'darkred')
+	abline(0, 1)
+	legend('topleft', legend = c(paste0('Lambda STAAR = ', round(lam0, 3)), paste0('Lambda sumSTAAR = ', round(lam1, 3))), pch = 19, bty = "n", col = c('black', 'darkred'), cex = .9)
+
+}
+
+#pdf('Fig.S5.pdf', height = 7.5)
+tiff('Fig.S5.tif', units="in", width=7, height=7.5, res=600, compression = 'lzw')
+
+par(mfrow = c(2, 2))
+
+pval0 <- sort(as.numeric(res0[, c('STAAR.B11', 'STAAR.B125')])) # sort() removes NAs
+pval1 <- sort(as.numeric(res0[, c('sumSTAAR.B11', 'sumSTAAR.B125')]))
+draw.qq(pval0, pval1, 'Burden tests')
+
+pval0 <- sort(as.numeric(res0[, c('STAAR.S11', 'STAAR.S125')]))
+pval1 <- sort(as.numeric(res0[, c('sumSTAAR.S11', 'sumSTAAR.S125')]))
+draw.qq(pval0, pval1, 'SKAT')
+
+pval0 <- sort(as.numeric(res0[, c('STAAR.A11', 'STAAR.A125')]))
+pval1 <- sort(as.numeric(res0[, c('sumSTAAR.A11', 'sumSTAAR.A125')]))
+draw.qq(pval0, pval1, 'ACAT')
+
+pval0 <- sort(as.numeric(res0[, 'STAAR.ACAT_O']))
+pval1 <- sort(as.numeric(res0[, 'sumSTAAR.ACAT_O']))
+draw.qq(pval0, pval1, 'Combined tests')
+
+dev.off()
